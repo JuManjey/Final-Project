22186817f8506cde3e007befa440db08cb06173e
jumanjey <opghld@gmail.com>
2022-06-02 22:35:31 +0200

    main

--- git-red.sh
+++ git-red.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-git config --global --add safe.directory /root/Documents/test_html/Final-Project/
+git config --global --add safe.directory /root/Downloads/Documents/test_html/Final-Project
 cd /var/www/html
 echo "REBASE"
 git rebase
